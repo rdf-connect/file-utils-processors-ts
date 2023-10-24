8e7e0c609b5980d5201d3ec71e59a99453de9fd8
Julian Rojas <julian.rojas87@gmail.com>
2023-10-24 15:31:32 +0200

    v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # file-utils-processors-ts
-Connector Architecture Typescript processors for handling files
+
+[![npm](https://img.shields.io/npm/v/file-utils-processors-ts.svg?style=popout)](https://npmjs.com/package/file-utils-processors-ts)
+
+Connector Architecture Typescript processors for handling file operations. It currently exposes 2 functions:
+
+* `js:globRead()`: This function relies on the [`glob`](https://www.npmjs.com/package/glob) library to select a set of files according to a shell expression and stream them out in a sequential fashion. A `wait` parameter can be defined to wait x milliseconds between file streaming operations.
+* `js:folderRead()`: This function reads all the files present in a given folder and streams out their content in a sequential fashion. A `maxMemory` parameter can be given to defined threshold of maximum used memory by the streaming process. When the threshold is exceeded, the streaming process will pause for 5 seconds (TODO: make configurable).
--- src/FileUtils.ts
+++ src/FileUtils.ts
@@ -1,9 +1,24 @@
 import { Writer } from "@treecg/connector-types";
 import path from "path";
-import { memoryUsage } from 'node:process';
+import { memoryUsage } from "node:process";
 import { access, readdir, readFile } from "fs/promises";
+import { glob } from "glob";
 
-export async function readFolder(folder: string, writer: Writer<string>, maxMemory?: number) {
+export async function globRead(globPattern: string, writer: Writer<string>, wait: number = 0) {
+    const jsfiles = await glob(globPattern, {});
+    const files = await Promise.all(
+        jsfiles.map((x) => readFile(x, { encoding: "utf8" })),
+    );
+
+    return async () => {
+        for (let file of files) {
+            await writer.push(file);
+            await new Promise((res) => setTimeout(res, wait));
+        }
+    };
+}
+
+export async function readFolder(folder: string, writer: Writer<string>, maxMemory: number = 3) {
     const normalizedPath = path.normalize(folder);
 
     // Check folder exists
@@ -21,12 +36,12 @@ export async function readFolder(folder: string, writer: Writer<string>, maxMemo
             // Monitor process memory to avoid high-water memory crashes
             // Currently limited to 3Gb
             console.log(`[PROCESSOR][readFolder] - processing ${fileName}`);
-            if (memoryUsage().heapUsed > (maxMemory ? maxMemory : 3) * 1024 * 1024 * 1024) {
+            if (memoryUsage().heapUsed > maxMemory * 1024 * 1024 * 1024) {
                 console.log(`[PROCESSOR][readFolder] - Phew! too much data (used ${Math.round((memoryUsage().heapUsed / 1024 / 1024) * 100) / 100} Mb)...waiting for 5s`);
                 await sleep(5000);
             }
 
-            writer.push((await readFile(path.join(normalizedPath, fileName))).toString());
+            await writer.push((await readFile(path.join(normalizedPath, fileName))).toString());
         }
     }, 5000);
 }
